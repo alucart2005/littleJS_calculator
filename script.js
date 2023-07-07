b8c91c7d37b2c80e5f6bdb3a03f4ea53196230ff
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-07 15:20:42 -0500

    feat: add step 5

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ buttons.forEach((item) => {
 // active toggle
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
-let isDark = true;
+//let isDark = true;
 themeToggleBtn.onclick=()=>{
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
-  isDark!=isDark
+  //isDark!=isDark
 } 
